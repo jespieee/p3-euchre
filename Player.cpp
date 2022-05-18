1e54121d09fb6f61f9e614009b982085ca6276c5
duffymit <duffymit@umich.eduexit>
2022-05-18 13:11:52 -0400

    Added main to euchre.cpp and sort of added function stubs into Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,40 @@
+// Project UID 1d9f47bfc76643019cfbf037641defe1
+
+#include <iostream>
+#include <string>
+#include "Player.h"
+Player * Player_factory(const std::string &name, const std::string &strategy) {
+  assert(false);
+}
+
+std::ostream & operator<<(std::ostream &os, const Player &p) {
+  assert(false);
+}
+
+/*const std::string & Player::get_name() const = 0;{
+    assert(false);
+}
+
+void Player::add_card(const Card &c) = 0{
+    assert(false);
+  }
+
+virtual bool Player::make_trump(const Card &upcard, bool is_dealer,
+                        int round, std::string &order_up_suit) const = 0{
+    assert(false);
+  }
+
+
+virtual void Player::add_and_discard(const Card &upcard) = 0{
+    assert(false);
+  }
+
+virtual Card Player::lead_card(const std::string &trump) = 0{
+    assert(false);
+  }
+
+virtual Player::Card play_card(const Card &led_card, const std::string &trump) = 0{
+    assert(false);
+  }
+*/
+
---
+++ euchre.cpp
@@ -0,0 +1,6 @@
+// Project UID 1d9f47bfc76643019cfbf037641defe1
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "hello from main!" << endl;
+}
