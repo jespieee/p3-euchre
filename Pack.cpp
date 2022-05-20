19a7589264c3fe4789300878dd06c59a154f80bf
mhuhu <mhuhu@umich.edu>
2022-05-20 01:10:24 -0400

    non functional pack

--- Pack.cpp
+++ Pack.cpp
@@ -10,7 +10,7 @@ Pack::Pack() {
     //int index_nine = 7;
     int suit_count = 0;
     for(int num = 0; num < 24; num++){
-        for(int rank_count = 7; rank_count < 12; rank_count++){
+        for(int rank_count = 7; rank_count <= 12; rank_count++){
             cards[num] = Card(RANK_NAMES_BY_WEIGHT[rank_count],SUIT_NAMES_BY_WEIGHT[suit_count]);
             num++;
         }
@@ -34,9 +34,9 @@ Pack::Pack(std::istream& pack_input) {
 }
 
 Card Pack::deal_one() {
-    Card wanted = cards[next];
-    next++;
-    return wanted;
+    int hold = next;
+    ++next;
+    return cards[hold];
 }
 
 void Pack::reset() {
@@ -45,28 +45,22 @@ void Pack::reset() {
 
 void Pack::shuffle() {
     //in shuffle 7 times
-    std::array<Card, PACK_SIZE> shuffled;
-    shuffled[0] = cards[12];
-    shuffled[1] = cards[0];
-    int counter2nd = 12;
-    int counter1st = 0;
-    for(int i = 0; i < 7; i++){
-        for (int j = 2; j < PACK_SIZE; j++) {
-            if (j % 2 == 0) {
-                shuffled[j] = cards[counter2nd];
-                counter2nd++;
-            }
-            else {
-                shuffled[j] = cards[counter1st];
-                counter1st++;
-            }
+    for(int i = 0; i < 7; i++) {
+        std::array<Card, 12> half2;
+        std::array<Card, 12> half1;
+        for (int j = 0; j < 12; j++) {
+            half1[j] = cards[j];
+        }
+        for (int k = 0; k < 12; k++) {
+            half2[k] = cards[k + 12];
+        }
+        for (int l = 0; l < 23; l += 2) {
+            cards[l] = half2[l / 2];
+            cards[l + 1] = half1[l / 2];
         }
         //go down each pile swapping positions of cards
     }
-    for (int i = 0; i < PACK_SIZE; i++) {
-        cards[i] = shuffled[i];
-    }
-    return;
+    reset();
 }
 
 
