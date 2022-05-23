47c3d5e821ba8d7e18be7d0a1d0f994ebb65780d
mhuhu <mhuhu@umich.edu>
2022-05-23 17:55:39 -0400

    progress of euchre, need pack tests

--- euchre.cpp
+++ euchre.cpp
@@ -112,38 +112,45 @@ public:
         upcard = pack.deal_one();
         cout << upcard << " turned up\n";
     }
-    void makeTrump() {
-        // start to left of dealer
-        for (int round = 1; round < 3; round++) {
-            int pos = nextPlayer(dealerCount);
-            for (int i = 0; i < 4; i++) {
-                if (pos == dealerCount && round == 1) {
-                    if (players[pos]->make_trump(upcard, true, round, trump)) {
-                        cout << players[pos] << " orders up " << trump << endl;
-                        players[pos]->add_and_discard(upcard);
-                        break;
-                    }
-                    else {
-                        cout << players[pos] << " passes\n";
-                    }
-                }
-                else {
-                    if (players[pos]->make_trump(upcard, false, round, trump)) {
-                        cout << players[pos] << " orders up " << trump << endl;
-                        if (round == 1) {
-                            players[pos]->add_and_discard(upcard);
-                        }
-                        break;
-                    }
-                    else {
-                        cout << players[pos] << " passes\n";
-                    }
-                }
-                pos = nextPlayer(pos);
-                // consider screw the dealer here
+	void makeTrump() {
+		// start to left of dealer
+		int pos = nextPlayer(dealerCount);
+		for (int i = 0; i < 4; i++) {
+			if (pos == dealerCount) {
+				if (players[pos]->make_trump(upcard, true, 1, trump)) {
+					cout << players[pos] << " orders up " << trump << endl;
+					players[pos]->add_and_discard(upcard);
+					break;
+				}
+				else {
+					cout << players[pos] << " passes\n";
+				}
+			}
+			else {
+				if (players[pos]->make_trump(upcard, false, 1, trump)) {
+					cout << players[pos] << " orders up " << trump << endl;
+					players[pos]->add_and_discard(upcard);
+					break;
+				}
+				else {
+					cout << players[pos] << " passes\n";
+				}
+			}
+			pos = nextPlayer(pos);
+			// consider screw the dealer here
+		}
+        for (int i = 0; i < 4; i++) {
+            if (players[pos]->make_trump(upcard, false, 2, trump)) {
+                cout << players[pos] << " orders up " << trump << endl;
+                break;
             }
+            else {
+                cout << players[pos] << " passes\n";
+            }
+            pos = nextPlayer(pos);
         }
-    }
+	}
+
 };
 int main(){
     cout << "hello from main!" << endl;
